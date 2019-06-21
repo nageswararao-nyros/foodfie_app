b61029683b13eb17c4283470ee0234363e3d9fa8
nageswararao-nyros <nageswarara-nyros@gmail.com>
2019-06-21 10:18:05 +0530

    Assets precompile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,6 @@
 //
 //= require jquery
 //= require jquery-ui
-//= require jquery_ujs
-//= require jquery.ui.touch-punch.min
 //= require autocomplete-rails
 // require turbolinks
 //= require bootstrap
